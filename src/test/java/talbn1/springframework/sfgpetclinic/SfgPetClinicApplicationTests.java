43a2a0dd9ac0ab6a610e07aa937e01e637fd9c28
talbn1 <talbn1@gmail.com>
2020-04-25 10:11:25 +0300

    Initial Project Add

---
+++ src/test/java/talbn1/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package talbn1.springframework.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
