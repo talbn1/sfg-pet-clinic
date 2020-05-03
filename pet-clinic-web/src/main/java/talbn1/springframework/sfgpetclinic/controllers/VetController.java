a6e3f302982311e30e4ced0ffcaf86a54ba0dec1
talbn1 <talbn1@gmail.com>
2020-05-03 16:18:01 +0300

    List all Vets on Vet Index page. closes #14

---
+++ pet-clinic-web/src/main/java/talbn1/springframework/sfgpetclinic/controllers/VetController.java
@@ -0,0 +1,15 @@
+package talbn1.springframework.sfgpetclinic.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+
+    @RequestMapping({"/vets", "/vets/index", "vets/index.html"})
+    public String listVets(){
+
+        return "vets/index";
+    }
+}
