d7dbf7f6dbb2827dd6e46993cb70a3065a51eede
talbn1 <talbn1@gmail.com>
2020-05-22 19:08:48 +0300

    Create Vet Type,Vet and Visit Entities  closes #24 #11

--- pet-clinic-data/src/main/java/talbn1/spring/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/talbn1/spring/petclinic/model/Vet.java
@@ -1,6 +1,17 @@
 package talbn1.spring.petclinic.model;
 
 
+import java.util.Set;
 
 public class Vet extends Person{
+
+    private Set<Specialty> specialities;
+
+    public Set<Specialty> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Specialty> specialities) {
+        this.specialities = specialities;
+    }
 }
