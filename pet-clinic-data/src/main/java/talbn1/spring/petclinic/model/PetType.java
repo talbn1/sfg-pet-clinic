0635c6a2ec51890235f1737c36a1d9c709ec31bf
talbn1 <talbn1@gmail.com>
2020-05-22 18:45:06 +0300

    Create Pet Type,Pet and Visit Entities  closes #23

--- pet-clinic-data/src/main/java/talbn1/spring/petclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/talbn1/spring/petclinic/model/PetType.java
@@ -7,4 +7,13 @@ package talbn1.spring.petclinic.model;
 public class PetType extends BaseEntity{
 
     private String name;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
